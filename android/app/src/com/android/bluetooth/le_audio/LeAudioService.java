8500a98e8240ffac10b4f300cd5cc5338b24f096
Shubham Singhal <quic_shubsing@quicinc.com>
2025-05-05 12:10:05 +0530

    Call not answered sometimes after sending Accept from inactive LE device
    Issue: When multiple devices connected i.e. RD1(A2dp) and RD2(SS LEA buds). RD1 is on A2dp streaming, and incoming call is coming on DUT. Call not answered sometimes from RD2 (SS LEA buds) even after remote send Call Accept.
    
    Fix: In the LeAudioService, AudioManagerAudioDeviceCallback callback came from MM audio once LEA device added when switching from RD1 to RD2 while answering the call. In the case of RD2 (SS LEA Buds) handleAudioDeviceAdded is returning true hence won't be able to again execute handleAudioDeviceAdded in the next iteration of for loop which is required to be run for each addedDevices. Hence making a change to execute handleAudioDeviceAdded for each added device, which will execute the cached CCP operation, when the handleAudioDeviceAdded is called with param isSource as true and once exposedActiveDevice is set to current device.
    
    CRs-Fixed: 4134564
    Change-Id: I9a51103ca5b20255140e8de5172284e192d04a6f

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -2457,10 +2457,8 @@ public class LeAudioService extends ProfileService {
                 if (deviceInfo.isSink()) {
                     mAudioManagerAddedOutDevice = device;
                 }
-                if (handleAudioDeviceAdded(
-                        device, deviceInfo.getType(), deviceInfo.isSink(), deviceInfo.isSource())) {
-                    return;
-                }
+                handleAudioDeviceAdded(device, deviceInfo.getType(),
+                                            deviceInfo.isSink(), deviceInfo.isSource());
             }
         }
 
