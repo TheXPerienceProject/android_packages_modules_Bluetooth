6425605fdcea76801c960178928dbe85a916e53c
Phaneendra Reddy <quic_phaneend@quicinc.com>
2025-05-19 15:27:49 +0530

    PTS: Updating proper metadata during streaming.
    when update metadata comes when ase's are already in streaming,
    we need to send the proper metadata whichever the PTS is
    expecting.
    
    CRs-Fixed: 4162046
    Change-Id: Iff71aee956090b608387654c96909fdb9354be51

--- system/bta/le_audio/state_machine.cc
+++ system/bta/le_audio/state_machine.cc
@@ -134,6 +134,8 @@ constexpr uint8_t LTV_LEN_MAX_FT = 0X01;
 
 constexpr uint8_t ENCODER_LIMITS_SUB_OP = 0x24;
 constexpr uint8_t HCI_VS_SET_CIG_CONTEXT_TYPE = 0x3C;
+static constexpr char kPtsCapAudioContextProp[] =
+  "persist.bluetooth.leaudio.pts.set.capAudio.context";
 
 typedef struct {
   uint8_t cig_id;
@@ -3484,8 +3486,17 @@ private:
         new_metadata = leAudioDevice->GetMetadata(directional_audio_context,
                                                   ccid_lists.get(ase->direction));
       } else {
-        new_metadata = leAudioDevice->GetMetadata(AudioContexts(LeAudioContextType::UNSPECIFIED),
-                                                  std::vector<uint8_t>());
+        uint64_t requiredCapAudioContext = 1; //UNSPECIFIED
+        requiredCapAudioContext = osi_property_get_int32(kPtsCapAudioContextProp, requiredCapAudioContext);
+        if (osi_property_get_bool("persist.bluetooth.leaudio.cap.pts", false)) {
+           log::debug("PTS execution for cap");
+           log::debug("required audio context is {}", requiredCapAudioContext);
+           new_metadata = leAudioDevice->GetMetadata(AudioContexts(requiredCapAudioContext),
+                                                     std::vector<uint8_t>());
+        } else {
+           new_metadata = leAudioDevice->GetMetadata(AudioContexts(LeAudioContextType::UNSPECIFIED),
+                                                     std::vector<uint8_t>());
+        }
       }
 
       /* Do not update if metadata did not changed. */
