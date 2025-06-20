c82558b852755722d15aec23164327aea334e9c7
sarvsaur <quic_sarvsaur@quicinc.com>
2025-06-20 11:08:20 +0530

    HFP: Send call indicators while ringing in DND mode
    Sending call indicators to remote while silent
    ringing in DND mode.
    
    CRs-Fixed: 4176351
    Change-Id: If786674ec08e3ebf6451df56746ea5f7d876e987

--- android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
+++ android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
@@ -2081,9 +2081,7 @@ public class BluetoothInCallService extends InCallService {
                     CallState.ALERTING;
 
             case Call.STATE_RINGING, Call.STATE_SIMULATED_RINGING -> {
-                if (call.isSilentRingingRequested()) {
-                    yield CallState.IDLE;
-                } else if (isForeground) {
+                if (isForeground) {
                     yield CallState.INCOMING;
                 } else {
                     yield CallState.WAITING;
