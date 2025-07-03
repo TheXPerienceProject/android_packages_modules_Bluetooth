da98cde3d41532f629de8d7f4647c765b04948bc
zhenchao <quic_zhenchao@quicinc.com>
2025-07-03 18:23:43 +0800

    VOIP WAR: reject startSco request if broadcasted inactive HFP device
    In some SHO corner case, SCO get connected in BT host stack, but HFP
    active device is still null in framework, so that BT_SCO=on can not
    be setting and audio will not route to BT SCO device.
    
    To resolve this issue, reject startSco request if broadcasted HFP device
    inactive.
    
    CRs-Fixed: 4202534
    Change-Id: Iaace8758c123374b0ee83e700c9ea503badfe008

--- android/app/src/com/android/bluetooth/le_audio/CallAudio.java
+++ android/app/src/com/android/bluetooth/le_audio/CallAudio.java
@@ -93,6 +93,7 @@ public class CallAudio {
     private BluetoothOnModeChangedListener mBluetoothOnModeChangedListener;
     private int mAudioMode = AudioManager.MODE_NORMAL;
     private boolean mDelayHfpActiveDeviceChange = false;
+    private BluetoothDevice mBroadcastedActiveDevice = null;
 
     public static int UNKNOWPROFILE = 0;
     public static int HFP = 1;
@@ -412,6 +413,7 @@ public class CallAudio {
             return;
         }
 
+        mBroadcastedActiveDevice = device;
         synchronized (headsetService) {
             BluetoothStatsLog.write(
                 BluetoothStatsLog.BLUETOOTH_ACTIVE_DEVICE_CHANGED,
@@ -426,6 +428,11 @@ public class CallAudio {
             headsetService.sendBroadcastAsUser(intent, UserHandle.ALL, BLUETOOTH_CONNECT,
                                                Utils.getTempBroadcastOptions().toBundle());
         }
+    }
+
+    public BluetoothDevice getBroadcastedActiveDevice() {
+        Log.d(TAG, "getBroadcastedActiveDevice, device: " + mBroadcastedActiveDevice);
+        return mBroadcastedActiveDevice;
     }
 
     private void broadcastAudioState(BluetoothDevice device, int fromState, int toState) {
@@ -609,7 +616,6 @@ public class CallAudio {
             && (state == BluetoothProfile.STATE_DISCONNECTING
                 || state == BluetoothProfile.STATE_DISCONNECTED)) {
             Log.d(TAG, "onConnStateChange: mActiveDevice disconnecting/disconnected");
-            updateActiveDevice(null, profile);
         }
 
         switch(otherProfileConnectionState) {
